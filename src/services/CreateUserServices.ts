86468eb7587cdc50f38bcd0dc9396324b44d40f1
kbath2 <kbjass2@gmail.com>
2020-09-10 11:00:08 -0300

    creation of user services

---
+++ src/services/CreateUserServices.ts
@@ -0,0 +1,32 @@
+import { hash } from 'bcryptjs';
+import IUserRepository from '../repositories/IUserRepository';
+import UseRepository from '../repositories/UseRepository';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  private userRepository: IUserRepository;
+
+  constructor(userRepository: UseRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const passwordHash = await hash(password, 8);
+
+    const user = await this.userRepository.create({
+      name,
+      email,
+      password: passwordHash,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
